d037ff76a3713983c00db2182e3b4c835c24d2f0
Ryuki Hashimoto <s1290184@u-aizu.ac.jp>
2022-07-12 14:29:28 +0900

    ユーザー名が追加されました

--- ex4.c
+++ ex4.c
@@ -4,7 +4,13 @@
 
 int main(){
     int i=0,couH=0,couT=0;
+    char name[100];
     srand((unsigned int)time(NULL));
+
+    printf("Who are you?\n");
+    printf("> ");
+    scanf("%s",name);
+    printf("Hello, %s!\n",name);
 
     printf("Tossing a coin...\n");
     for(i=1 ; i<4 ; i++){
@@ -18,6 +24,6 @@ int main(){
             couT++;
         }
     }
-    printf("Heads: %d, Tails: %d\n");
+    printf("Heads: %d, Tails: %d\n",couH,couT);
     return 0;
 }
