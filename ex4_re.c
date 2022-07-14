85dda109e4348021ded6c9ec077a4600f46981ce
Ryuki Hashimoto <s1290184@u-aizu.ac.jp>
2022-07-14 11:55:25 +0900

    HoT: initial revision

---
+++ ex4_re.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    int i=0,couH=0,couT=0;
+    int x;
+    srand((unsigned int)time(NULL));
+
+    printf("Tossing a coin...\n");
+    for(i=1 ; i<4 ; i++){
+        x = rand()%2;
+        if(x==0){
+            printf("Round %d: Heads\n",i);
+            couH++;
+        }
+        else if(x==1){
+            printf("Round %d: Tails\n",i);
+            couT++;
+        }
+    }
+    printf("Heads: %d, Tails: %d\n",couH,couT);
+    
+    return 0;
+}
